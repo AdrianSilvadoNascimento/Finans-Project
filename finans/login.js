d8bf539566bfed9e4ef9f7a00f299022006ff382
AdrianSilvadoNascimento <75761636+AdrianSilvadoNascimento@users.noreply.github.com>
2021-04-08 10:09:18 -0300

    Página de Cadastrar e fazer Entrar - Login
    Esta página foi construída para adicionar uma nova pessoa na lista de usuários e verificar se já uma conta com o mesmo usuário e senha. Se não houver, adiciona um novo usuário. 
    This Page was built to add a new user and verify another user in the array of accounts.

---
+++ finans/login.js
@@ -0,0 +1,29 @@
+var listaContas = ["Adrian", 123];
+
+var usuario = document.getElementById('name');
+var senha = document.getElementById('password');
+
+var cadastrou = document.getElementById('sucesso');
+var cadastrado = false;
+
+cadastrou.style = "display:none";
+
+function adicionarNaLista(){
+    var name = usuario.value;
+    var password = senha.value;
+    if(name == listaContas[0] && password == listaContas[1]){
+        console.log(name + " Você já está cadastrado!");
+        cadastrado = true;
+        usuario.value = "";
+    }
+    
+    if(cadastrado == true){
+        alert(name + " Você já está cadastrado!")
+        usuario.value = "";
+        cadastrado = false;
+    }else if (name != listaContas[0] && password != listaContas[1] && cadastrado == false){
+        listaContas.push(name, password);
+        cadastrou.style = "display:block";
+        console.log(listaContas);
+    }
+}
